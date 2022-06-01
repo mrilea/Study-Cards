b7df474690fcd8e4eca8d86c713fce3f762e1bd6
Marlin Rilea <94160218+mrilea@users.noreply.github.com>
2022-05-31 21:34:15 -0700

    Update Header.js

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header className="jumbotron bg-dark">
       <div className="container text-white">
-        <h1 className="display-4">Flashcard-o-matic</h1>
-        <p className="lead">Discover The Flashcard Difference.</p>
+        <h1 className="display-4">Study Cards</h1>
+        <p className="lead">A customizable studying solution.</p>
       </div>
     </header>
   );
